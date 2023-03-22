c7ec7a5dcb581df8c684c82a0f84fdb0a57d6cd2
CosminIordache <cosminionutiordache@gmail.com>
2023-03-22 09:32:16 +0100

    darkmode

--- app/src/main/java/com/example/apimovies/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/apimovies/ui/view/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.apimovies.ui.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.apimovies.R
 import com.example.apimovies.data.ApiService
 import com.example.apimovies.model.Movie
 import com.example.apimovies.data.Util
 import com.example.apimovies.databinding.ActivityMainBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
 import kotlinx.coroutines.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -27,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
 //        val content : View = findViewById(androidx.appcompat.R.id.content)
 //        content.viewTreeObserver.addOnPreDrawListener(
@@ -57,6 +67,8 @@ class MainActivity : AppCompatActivity() {
         adapter = MoviesAdapter(moviesImagen)
         binding.recycleView.adapter = adapter
 
+        // onClick in a itemView gives all info and go to another activity
+
         adapter.onClick = {
             val i = Intent(this, DetailsActivity::class.java)
             val imagen = moviesImagen.get(binding.recycleView.getChildAdapterPosition(it)).image
@@ -70,7 +82,7 @@ class MainActivity : AppCompatActivity() {
                 moviesImagen.get(binding.recycleView.getChildAdapterPosition(it)).vote_average
 
 
-            // pass the transition name and other data to the intent
+            // gives all the attributes that have a movie
             i.putExtra("image", imagen)
             i.putExtra("title", title)
             i.putExtra("overview", overview)
@@ -89,7 +101,6 @@ class MainActivity : AppCompatActivity() {
 
     private suspend fun getMoviesAPI() {
         //CoroutineScope(Dispatchers.IO).launch {
-
             val call = getRetrofit().create(ApiService::class.java)
                 .getMovies(key = "9fbea8d0a3e8c5d1ab5573cb33b9282c", leng = "en-US")
 
@@ -118,6 +129,34 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         //}
+    }
+
+    override fun onCreateOptionsMenu(
+        menu: Menu?
+    ): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        val switchItem = menu!!.findItem(R.id.menu_switch)
+        val switchView : SwitchMaterial= switchItem?.actionView!!.findViewById(R.id.switch_item)
+
+        switchView.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                enableDarkMode()
+                switchView.isChecked = true
+            } else {
+                disableDarkMode()
+                switchView.isChecked = false
+            }
+        }
+
+        return true
+    }
+
+    private fun enableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+    }
+
+    private fun disableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
     }
 
 }
